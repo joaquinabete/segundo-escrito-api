6c85c74eb86ba44ed168ab46fbc5a555e2fecf82
Joaquin Abete <joaquinsini@gmail.com>
2024-07-20 17:45:33 -0300

    Se fusionan las ramas y se pushean los cambios

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Persona;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Validator; //
 
 class PersonaController extends Controller
 {
